efc486c8f7e13b11e7e46d137a54d17d39bc39ef
DuongDuc210 <duonganhduc6a4@gmail.com>
2024-11-14 19:23:52 +0700

    done

--- src/page/(website)/product_detail/components/RelatedProductDetail.tsx
+++ src/page/(website)/product_detail/components/RelatedProductDetail.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { GoArrowSwitch } from "react-icons/go";
 import { MdShare } from "react-icons/md";
 import { RiHeart3Line } from "react-icons/ri";
